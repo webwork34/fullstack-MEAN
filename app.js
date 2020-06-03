02a8f981bc45782ca86185d87f327e8654f0c6bd
Kostiantyn Marych <web.work.34@gmail.com>
2020-06-03 23:00:51 +0300

    fix bag

--- app.js
+++ app.js
@@ -39,10 +39,10 @@ app.use('/api/category', categoryRout);
 app.use('/api/order', orderRout);
 app.use('/api/position', positionRout);
 
-if (process.env.NODE_ENV = 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/dist/client'));
 
-  app.get('*', (req,res) => {
+  app.get('*', (req, res) => {
     res.sendFile(
       path.resolve(
         __dirname, 'client', 'dist', 'client', 'index.html'
